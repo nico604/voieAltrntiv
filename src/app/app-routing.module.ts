944958ed464c517c0bdfc981fb0e12ada6a31cdc
nico604 <nicolas.girard.qc@gmail.com>
2020-07-14 01:48:24 -0400

    migration terminé

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CrewComponent } from './crew/crew.component';
+import { EvenementsComponent } from './evenements/evenements.component';
+import { ContactComponent } from './contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: 'crew', component: CrewComponent },
+	{ path: 'evenements', component: EvenementsComponent },
+	{ path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CrewComponent } from './crew/crew.component';
+import { EvenementsComponent } from './evenements/evenements.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    CrewComponent,
+    EvenementsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
